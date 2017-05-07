980228d08d84509b9c18573437d0bdcff6522e91
JulianChu <yulang.chu@gmail.com>
2017-05-07 17:23:53 +0200

    Code first: add TodoMvcDbContext

---
+++ TodoMVC_WebAPI/Models/TodoMvcDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TodoMVC_WebAPI.Models
+{
+    public class TodoMvcDbContext:DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public TodoMvcDbContext():base("name=LocalConnection")
+        {
+
+        }
+        
+    }
+}
